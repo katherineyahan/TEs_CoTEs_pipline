d0425de7c448c51354001c07e71e7adce096a2c5
Yahan Zhang <zyhkatherine0609@outlook.com>
2023-11-20 17:15:54 -0500

    Updated

--- code/CoTEs_Biological_Validation.R
+++ code/CoTEs_Biological_Validation.R
@@ -2,7 +2,7 @@ library(ChIPseeker)
 library(TxDb.Hsapiens.UCSC.hg38.knownGene)
 library(org.Hs.eg.db)
 txbd<-TxDb.Hsapiens.UCSC.hg38.knownGene
-peak <- readPeakFile('D:/TEs_CoTEs_pipline/data/V2-TEs/non_olap/cores1000/85_Alu_Merged.bed')
+peak <- readPeakFile('D:/TEs_CoTEs_pipline/data/V2-TEs/non_olap/cores1000/542_L1MA1_Merged.bed')
 covplot(peak)
 peakAnno<-annotatePeak(peak,tssRegion=c(-3000,3000),TxDb=txbd,annoDb="org.Hs.eg.db")
 plotAnnoPie(peakAnno)
